5e3437a7777261c549d32cdd542677abbbaa0d90
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-02 23:43:37 +0545

    assignment of canvas added

---
+++ Code_Course/Assignments/TrafficLights/script.js
@@ -0,0 +1,332 @@
+let canvas= document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+
+// Sky
+context.beginPath();
+context.rect(0,0,300,300);
+context.fillStyle="	#243447";
+context.fill();
+
+
+//moon
+context.beginPath();
+context.fillStyle="silver";
+context.arc(280,10,20,0,2*Math.PI);
+context.fill();
+
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(12,11,2,0,2*Math.PI);
+context.fill();
+
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(12,11,2,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(60,45,2,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(60,145,2,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(260,145,2,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(12,110,1,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(182,67,1,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(80,110,2,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(220,69,2,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(243,200,1,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(183,120,1,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(160,100,1,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(175,60,1,0,2*Math.PI);
+context.fill();
+//star
+context.beginPath();
+context.fillStyle="silver";
+context.arc(150,10,1,0,2*Math.PI);
+context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(32,20,3.5,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(28,20,3,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(25,20,2.5,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(26,20,2,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(16,20,1.5,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(13,20,1,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(11,20,0.5,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(9,20,0.4,0,2*Math.PI);
+// context.fill();
+// //comet
+// context.beginPath();
+// context.fillStyle="silver";
+// context.arc(5,20,0.3,2*Math.PI);
+// context.fill();
+
+
+
+
+
+
+
+
+
+
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(0,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(20,210,20,30);
+context.stroke();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(40,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(60,210,20,30);
+context.stroke();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(80,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(100,210,20,30);
+context.stroke();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(120,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(140,210,20,30);
+context.stroke();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(160,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(180,210,20,30);
+context.stroke();// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(200,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(220,210,20,30);
+context.stroke();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(240,210,20,30);
+context.stroke();
+context.fill();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.lineWidth=5;
+context.rect(260,210,20,30);
+context.stroke();
+// create bar
+context.beginPath();
+context.strokeStyle="black" ;
+context.fillStyle="green";
+context.lineWidth=5;
+context.rect(280,210,20,30);
+context.stroke();
+context.fill();
+
+
+
+
+
+// create background box
+context.beginPath();
+context.fillStyle="	black";
+context.rect(20,70,100,30);
+context.fill();
+
+//create red
+context.beginPath();
+context.fillStyle="red";
+context.arc(39,85,10,0,2*Math.PI);
+context.fill();
+
+//create Orange
+context.beginPath();
+context.fillStyle="orange";
+context.arc(70,85,10,0,2*Math.PI);
+context.fill();
+
+//create Green
+context.beginPath();
+context.fillStyle="green";
+context.arc(100,85,10,0,2*Math.PI);
+context.fill();
+
+
+// create pole
+context.beginPath();
+context.fillStyle="black";
+context.rect(65,100,10,150);
+context.fill();
+
+
+// create road
+context.beginPath();
+context.fillStyle="	black";
+context.rect(0,240,300,60);
+context.fill();
+
+// create divider
+context.beginPath();
+context.fillStyle="white";
+context.rect(10,280,30,10);
+context.fill();
+
+// create divider
+context.beginPath();
+context.fillStyle="white";
+context.rect(60,280,30,10);
+context.fill();
+
+// create divider
+context.beginPath();
+context.fillStyle="white";
+context.rect(110,280,30,10);
+context.fill();
+
+// create divider
+context.beginPath();
+context.fillStyle="white";
+context.rect(160,280,30,10);
+context.fill();
+
+// create divider
+context.beginPath();
+context.fillStyle="white";
+context.rect(210,280,30,10);
+context.fill();
+
+// create divider
+context.beginPath();
+context.fillStyle="white";
+context.rect(250,280,30,10);
+context.fill();
+
+
+ 
+
+
+
--- Code_Course/Day01_Pablo_Picasso/Canvas/canvas.js
+++ Code_Course/Day01_Pablo_Picasso/Canvas/canvas.js
@@ -1,27 +1,23 @@
-let canvas = document.getElementById("Canvas"); // paper
+let canvas = document.getElementById("canvas"); // paper
 let context = canvas.getContext("2d"); //pen
 
 
 // drawing Rectangle
 context.beginPath();
-context.fillStyle(red);
 context.rect(0,0,50,50);
 context.fill();
 
 context.beginPath();
-context.fillStyle(black);
 context.rect(60,0,50,50);
 context.fill();
 
 context.beginPath();
-context.fillStyle(blue);
-context.rect(60,0,50,50);
+;
+context.rect(120,0,50,50);
 context.fill();
 
 
 // Drawing Circle
 context.beginPath();
-context.fillStyle(black);
-// context.arc(60,0,0,360);
-context.arc(80,30,20,0,2*Math.PI);
+context.arc(80,100,20,0,2*Math.PI);
 context.fill();
