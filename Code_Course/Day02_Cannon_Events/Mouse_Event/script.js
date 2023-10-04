7e0666593c35b750814d8921dbbbed87832fb17a
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-04 12:35:20 +0545

    mouse and key event added

--- Code_Course/Day02_Cannon_Events/Mouse_Event/script.js
+++ Code_Course/Day02_Cannon_Events/Mouse_Event/script.js
@@ -1,18 +1,31 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-context.beginPath();
-context.rect(60,0,50,50);
-context.fill();
+
 
 canvas.addEventListener("mousemove",(event) =>{
     console.log(event.clientX,event.clientY);
 
     context.clearRect(0,0,300,300);
-
+    // outer circle
     context.beginPath();
     context.fillStyle="red";
-    context.arc(event.clientX,event.clientY,20,0,360);
+    context.arc(event.clientX,event.clientY,18,0,360);
+    context.fill();
+    // middle circle
+    context.beginPath();
+    context.fillStyle="yellow";
+    context.arc(event.clientX,event.clientY,13,0,360);
+    context.fill();
+    // inner core circle
+    context.beginPath();
+    context.fillStyle="orange";
+    context.arc(event.clientX,event.clientY,8,0,360);
+    context.fill();
+    // dot
+    context.beginPath();
+    context.fillStyle="black";
+    context.arc(event.clientX,event.clientY,3,0,360);
     context.fill();
 });
 
