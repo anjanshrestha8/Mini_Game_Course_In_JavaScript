92cfcbdd2d44389fd886b6b3bb7b22e186348aed
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-04 13:02:36 +0545

    added assignment

--- Assignments/Follow_Me/script.js
+++ Assignments/Follow_Me/script.js
@@ -12,7 +12,21 @@ canvas.addEventListener("mousemove",(event) =>{
     context.fillStyle="red";
     context.arc(event.clientX,event.clientY,18,0,360);
     context.fill();
-   
+    // middle circle
+    context.beginPath();
+    context.fillStyle="yellow";
+    context.arc(event.clientX,event.clientY,13,0,360);
+    context.fill();
+    // inner core circle
+    context.beginPath();
+    context.fillStyle="orange";
+    context.arc(event.clientX,event.clientY,8,0,360);
+    context.fill();
+    // dot
+    context.beginPath();
+    context.fillStyle="black";
+    context.arc(event.clientX,event.clientY,3,0,360);
+    context.fill();
 });
 
 document.addEventListener("keydown", (event) =>{
