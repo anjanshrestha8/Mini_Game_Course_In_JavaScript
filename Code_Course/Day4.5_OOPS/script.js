dfccc659dcf893df1328d8cd3d912d3d56586c6b
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-06 20:31:06 +0545

    box simulation

--- Code_Course/Day4.5_OOPS/script.js
+++ Code_Course/Day4.5_OOPS/script.js
@@ -3,14 +3,25 @@ const c = canvas.getContext("2d");
 
 class Box{
     constructor(){
-
+        this.x =0;
+        this.y=0;
+        this.width =40;
+        this.height =40;
+    }
+    draw(){
+        c.beginPath();
+        c.rect(this.x,this.y,this.width,this.height);
+        c.fill();
     }
 }
 
 const box = new Box();
 
+
 function animate(){
     // update logic here
+    c.clearRect(0,0,canvas.width,canvas.height);
+    box.draw();
     
     requestAnimationFrame(animate);
 
