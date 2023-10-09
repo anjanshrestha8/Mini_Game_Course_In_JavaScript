3e819af76f0a5c7a4ac4e52fade02a8accdb897d
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-09 18:15:22 +0545

    animate files added

--- Project/Animate/script.js
+++ Project/Animate/script.js
@@ -1,46 +1,53 @@
 const canvas = document.getElementById("canvas");
 const c =canvas.getContext("2d");
 
-
 let particleArray =[];
 let totalNumber =30;
-
-    let mouseX =0;
-    let mouseY =0;
+let cond=false;
 
 
-    window.addEventListener("mousemove",(event) => {
-        console.log(event);
-         mouseX = event.clientX;
-         mouseY = event.clientY;
-    
-    
-    });
+//let r =Math.random()*(30-10) + 10; 
+let rb=0
+
+
+canvas.width= 1000;
+canvas.height= 500;
+
+
+   
 class Particle{
     constructor(color){
         this.x = Math.random()*500;
         this.y =Math.random()*500;
-        this.r = Math.random()*(30-10) + 10;
+        this.r=Math.random()*(30-10) + 10; 
         this.x_speed=3;
         this.y_speed=3;
         this.color=color||"white";
     }
 
     draw(){
+       
+
         c.beginPath();
         c.fillStyle=this.color,0.04;
-        c.arc(this.x,this.y,this.r,0,360);
+        c.arc(this.x,this.y,this.r+rb,0,360);
         c.fill();
     }
     move(){
         this.y= this.y+this.y_speed;
         this.x= this.x+this.x_speed;
-        if(mouseX - this.x < 50 && mouseX - this.x > - 50){
-            this.r +=1;
-        console.log(this.r);
-
-            
+    //     if(mouseX - this.x < 50 && mouseX - this.x > - 50){
+    //         this.r +=1;
+    //     console.log(this.r);
+    //  }
+    if(cond)
+    {
+        rb-=2
+        if(rb<=5)
+        {
+            cond=false;
         }
+    }
 
     }
     checkCollision(){
@@ -57,7 +64,7 @@ class Particle{
 
         }
         //left
-        if(this.x + this.r > canvas.width){
+        if(this.x +this. r > canvas.width){
             this.x_speed-= 3;
             this.color="red";
 
@@ -79,20 +86,40 @@ class Particle{
 
     }
 } 
-for(let i=0;i<=totalNumber;i++){
-    particleArray.push(new Particle);
 
-}
 function animate(){
-    c.clearRect(0,0,500,500);
+    c.clearRect(0,0,canvas.width,canvas.height);
+    // for(let i=0;i<=totalNumber;i++){
+    //     particleArray[i].update();
+    // }
     for(let i=0;i<=totalNumber;i++){
+        particleArray.push(new Particle);
         particleArray[i].update();
+    
+    
     }
 
     requestAnimationFrame(animate);
 
 }
 animate();
+
+canvas.addEventListener("mousemove",(event) => {
+    console.log(event);
+     mouseX = event.clientX;
+     mouseY = event.clientY;
+     rb +=1;
+
+
+
+});
+canvas.addEventListener("mouseleave",(event) =>{
+    cond=true;
+   
+      
+    
+    
+});
     
 
 
