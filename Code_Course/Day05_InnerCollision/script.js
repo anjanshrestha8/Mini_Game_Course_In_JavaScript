e24c3d3024fb1997ad14fbeb80733d3065de748e
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-11 12:15:22 +0545

    inner collision code added

--- Code_Course/Day05_InnerCollision/script.js
+++ Code_Course/Day05_InnerCollision/script.js
@@ -1,4 +1,94 @@
 const canvas = document.getElementById('canvas')
 const c = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 500;
+let color ='black'
 
-clas
+let boxs =[];
+
+class Box{
+    constructor(){
+        this.position={x:Math.random()*450,y:Math.random()*450};
+        this.size ={width:50,height:50}
+        this.velocity ={x:1,y:1}
+        this.acceleration=0.4;
+    }
+    // draw rect
+    draw(){
+        c.beginPath();
+        c.fillStyle = color;
+        c.rect(this.position.x,this.position.y,this.size.width,this.size.height);
+        c.fill();
+    }
+    //move rect
+    move(){
+        this.velocity.y = this.velocity.y + this.acceleration;
+        this.position.y =this.position.y +this.velocity.y;
+        // this.velocity.x = this.velocity.x + this.acceleration;
+        // this.position.x =this.position.x +this.velocity.x;
+    }
+    // add gravity on it
+    borderCollision(){
+        if(this.position.y + this.size.height > canvas.height){
+            this.position.y =canvas.height -this.size.height;
+            this.velocity.y = this.velocity.y * - 0.4;
+        }
+        if(this.position.y < 0){
+            // this.position.y = canvas.height + this.height;
+            this.velocity.y = this.velocity.y * - 0.4;
+            this.acceleration = 0.8;
+
+        }
+
+    }
+    // collision among boxes
+    innerCollision(){
+        if(x + 50 > this.position.x  && this.position.x+50 > x && y+50 > this.position.y && this.position.y +50 >y ){
+        this.velocity.y = this.velocity.y - 2;
+
+        this.position.y =this.position.y + this.velocity.y;
+            
+           
+        }
+        else{
+            color="blue"
+        }
+    }
+    // call all function
+    update(){
+        this.draw();
+        this.move();
+        this.borderCollision();
+        this.innerCollision();
+    }
+}
+
+let x ;
+let y ;
+
+canvas.addEventListener("mousemove" , (event) =>{
+    x = event.clientX;
+    y = event.clientY;
+
+});
+
+for(let i =0;i<=10;i++){
+    boxs.push(new Box());
+}
+
+// game loop
+function animate(){
+
+    c.clearRect(0,0,canvas.width,canvas.height);
+    for(let i=0;i<=10;i++){
+        boxs[i].update();
+    }
+    // new rect made using mouse hover
+    c.beginPath();
+    c.rect(x,y,50,50);
+    c.fillStyle ="red"
+    c.fill();
+    requestAnimationFrame(animate);
+}
+animate();
+
