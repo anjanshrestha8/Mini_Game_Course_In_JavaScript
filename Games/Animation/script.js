183e38d48c5469fc669790894ba0c73c26c7c9da
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-12 20:10:00 +0545

    another game day 7 space shooter

--- Games/Animation/script.js
+++ Games/Animation/script.js
@@ -8,12 +8,27 @@ const spriteHeight="523";
 
 const playerImage = new Image();
 playerImage.src='shadow_dog.png';
-
+let frameX =0;
+let frameY =4 ;
+let gameFrame =0;
+let delay =4;
 
 function animate(){
     c.clearRect(0,0,canvas.width,canvas.height);
-   //c.drawImage(image,sx,sx,swidth,sheight,dx,dy,dwidth,dheight);   9 parameters in adding image
-    c.drawImage(playerImage,0,0,spriteWidth,spriteHeight,0,0,canvas.width,canvas.height);
+    //c.drawImage(image,sx,sx,swidth,sheight,dx,dy,dwidth,dheight);   9 parameters in adding image
+    c.drawImage(playerImage,frameX * spriteWidth,frameY * spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+    if(gameFrame % delay == 0){
+         if(frameX < 6)
+        {
+            frameX++;
+        } 
+        else 
+        {
+            frameX =0;
+        }  
+    }
+  
+    gameFrame ++;
     requestAnimationFrame(animate);
 }
 animate();
