b9df7a743ad700d3b5865167f58a714c923a8fb8
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-12 15:19:45 +0545

    game coded

--- Games/Animation/script.js
+++ Games/Animation/script.js
@@ -3,6 +3,9 @@ const c = canvas.getContext('2d');
 canvas.width = "600";
 canvas.height = "600";
 
+const spriteWidth ="575";
+const spriteHeight="523";
+
 const playerImage = new Image();
 playerImage.src='shadow_dog.png';
 
@@ -10,7 +13,7 @@ playerImage.src='shadow_dog.png';
 function animate(){
     c.clearRect(0,0,canvas.width,canvas.height);
    //c.drawImage(image,sx,sx,swidth,sheight,dx,dy,dwidth,dheight);   9 parameters in adding image
-    c.drawImage(playerImage,0,0,canvas.width,canvas.height);
+    c.drawImage(playerImage,0,0,spriteWidth,spriteHeight,0,0,canvas.width,canvas.height);
     requestAnimationFrame(animate);
 }
 animate();
