b7363508fb412f762aea9c1f8b5f58ab6a37d040
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-10-28 14:43:04 +0545

    new prject added

--- Code_Course/Day08_FlappyBird/pipe.js
+++ Code_Course/Day08_FlappyBird/pipe.js
@@ -17,18 +17,15 @@ class Pipe{
         this.img = new Image();
         this.img.src ="./pipe-down.png"
     }
-        // draw upper pipe
+    // draw upper pipe
     drawUpper(){
         c.beginPath();
         c.drawImage(this.image,this.position.x,this.position.y,this.size.width,this.size.height);
-
-        
     }
     // draw lower pipe
     drawLower(){
         c.beginPath();
         c.drawImage(this.img,this.position.x ,this.position.y +this.size.height +150,this.size.width,this.size.height)
-       
     }
     checkCollision(){
         if(this.position.x + this.size.width >= bird.position.x - bird.size.width/2 &&
