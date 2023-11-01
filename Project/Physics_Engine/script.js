c5c5795df900a0d79191884bb286dfa7501c20f1
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-01 18:39:37 +0545

    files modified

--- Project/Physics_Engine/script.js
+++ Project/Physics_Engine/script.js
@@ -5,25 +5,50 @@ canvas.height =400;
 
 class Cannon{
     constructor(){
-        this.position ={x: 10,y:350}
-        this.size={width:50,height:50}
+        this.position ={x: 10,y:295}
+        this.size={width:70,height:60}
         this.velocity={x:1,y:1}
-        this.acceleration=0.4;
+        this.acceleration=0.2;
+        this.can = new Image();
+        this.can.src="./cannon1.png"
     }
     draw(){
         c.beginPath();
         c.fillStyle="black"
-        c.fillRect( this.position.x,this.position.y,this.size.width,this.size.height);
+        c.drawImage( this.can,this.position.x,this.position.y,this.size.width,this.size.height);
         console.log("draw")
     }
+    drawStand(){
+        c.beginPath();
+        c.moveTo(45,350)
+        c.lineTo(25,400);
+        c.stroke();
+
+        c.beginPath();
+        c.moveTo(45,350)
+        c.lineTo(65,400);
+        c.stroke();
+
+    }
+    update(){
+        this.draw();
+        this.drawStand();
+    }
 }
+let mouseX;
+let mouseY;
+
+canvas.addEventListener("mousemove",(event) =>{
+    mouseX=event.clientX;
+    console.log(event.clientX)
+})
 let cannon = new Cannon();
 
 //game loop()
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
-    cannon.draw();
+    cannon.update();
     
 }
 animate();
