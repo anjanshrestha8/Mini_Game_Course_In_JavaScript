5bb576ffbb42ae14a60c574e1d4fc7138387e2ca
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-02 23:58:37 +0545

    game loop added

--- Games/My_First_Game/script.js
+++ Games/My_First_Game/script.js
@@ -4,3 +4,11 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+
+// game loop
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,canvas.width,canvas,height)
+}
+animate();
+
