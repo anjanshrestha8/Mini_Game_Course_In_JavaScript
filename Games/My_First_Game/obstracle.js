99136c5866191e7b701b62af08bc47bfe2df3e16
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-03 17:54:29 +0545

    enemy file added

--- Games/My_First_Game/obstracle.js
+++ Games/My_First_Game/obstracle.js
@@ -2,7 +2,7 @@ class Enemy{
     constructor(){
         this.position={x:550,y:550}
         this.size ={width:50,height:50}
-        this.velocity ={x:1,y:1}
+        this.velocity ={x:5,y:1}
     }
     draw(){
         c.beginPath();
@@ -10,10 +10,18 @@ class Enemy{
         c.fillRect(this.position.x,this.position.y,this.size.width,this.size.height)
     }
     move(){
-        this.position.x =this.position.x-this.velocity.x
+        this.position.x =this.position.x- this.velocity.x
+    }
+    checkCollision(){
+        if(this.position.x<0){
+            this.position.x=550;
+            this.size.height=Math.random()*50
+            this.position.y = canvas.height-this.size.height;
+        }
     }
     update(){
         this.draw();
         this.move();
+        this.checkCollision();
     }
 }
