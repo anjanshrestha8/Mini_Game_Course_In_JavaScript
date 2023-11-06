9ede2e9bda837e025602f703b3afcda08b8233ee
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-07 00:27:29 +0545

    file modified

--- Games/My_First_Game/script.js
+++ Games/My_First_Game/script.js
@@ -4,19 +4,39 @@ const c = canvas.getContext('2d')
 canvas.width = 600;
 canvas.height = 600;
 
+function background(){
+    c.beginPath();
+    c.fillStyle="grey"
+    c.fillRect(0,550,600,50);
+}
+
 class Dragon{
     constructor(){
         this.position={x:10,y:10};
-        this.size={width:10,length:10};
+        this.size={width:50,height:50};
         this.velocity={x:1,y:1};
         this.acceleration=0.8;
     }
     draw(){
         c.beginPath();
-        c.fillStyle="red"
-        c.fillRect=(this.position.x,this.position.y,this.size.width,this.size.height);
+        c.fillStyle="black"
+        c.rect(this.position.x,this.position.y,this.size.width,this.size.height);
+        c.fill();
+        console.log("draw")
+    }
+    update(){
+        this.draw();
     }
 } 
-let obj 
+let obj = new Dragon();
+
+function animate(){
+    c.clearRect(0,0,canvas.width,canvas.height);
+    obj.update();
+    background();
+    requestAnimationFrame(animate);
+}
+
+animate();
 
 
