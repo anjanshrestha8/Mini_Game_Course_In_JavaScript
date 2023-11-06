a9a619f2155ee694efb272f1c5e29c618e250f38
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-07 00:14:52 +0545

    unwanted file added

--- Games/My_First_Game/script.js
+++ Games/My_First_Game/script.js
@@ -11,6 +11,12 @@ class Dragon{
         this.velocity={x:1,y:1};
         this.acceleration=0.8;
     }
+    draw(){
+        c.beginPath();
+        c.fillStyle="red"
+        c.fillRect=(this.position.x,this.position.y,this.size.width,this.size.height);
+    }
 } 
+let obj 
 
 
