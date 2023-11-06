188fdde01acc54e7867a61782af11675f70915e6
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-07 00:13:43 +0545

    unwanted file added

--- Games/My_First_Game/script.js
+++ Games/My_First_Game/script.js
@@ -4,5 +4,13 @@ const c = canvas.getContext('2d')
 canvas.width = 600;
 canvas.height = 600;
 
+class Dragon{
+    constructor(){
+        this.position={x:10,y:10};
+        this.size={width:10,length:10};
+        this.velocity={x:1,y:1};
+        this.acceleration=0.8;
+    }
+} 
 
 
