2e21d7720b3be7eb102ec9783dc666d422099838
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-20 22:12:21 +0545

    failed to crack jump effect

--- Games/My_First_Game/obstracle.js
+++ Games/My_First_Game/obstracle.js
@@ -1,12 +1,12 @@
 class Enemy{
     constructor(){
-        this.position={x:550,y:550}
+        this.position={x:550,y:500}
         this.size ={width:50,height:50}
         this.velocity ={x:5,y:1}
     }
     draw(){
         c.beginPath();
-        c.fillStyle="red"
+        c.fillStyle="white"
         c.fillRect(this.position.x,this.position.y,this.size.width,this.size.height)
     }
     move(){
@@ -14,9 +14,9 @@ class Enemy{
     }
     checkCollision(){
         if(this.position.x<0){
-            this.position.x=550;
+            this.position={x:550,y:500}
             this.size.height=Math.random()*50
-            this.position.y = canvas.height-this.size.height;
+            
         }
     }
     update(){
--- Games/My_First_Game/script.js
+++ Games/My_First_Game/script.js
@@ -8,40 +8,68 @@ function background(){
     c.beginPath();
     c.fillStyle="grey"
     c.fillRect(0,550,600,50);
+    
 }
 
 class Dragon{
-    constructor(){
-        this.position={x:300,y:10};
-        this.size={width:50,height:50};
-        this.velocity={x:1,y:1};
-        this.acceleration=0.8;
+    constructor(x,y,size,color){
+        this.position={x:x,y:y};
+        this.size=size;
+        this.color=color;
+        this.velocity=12;
+        this.acceleration=1;
+       this.jumpPressed=false;
     }
     draw(){
         c.beginPath();
-        c.fillStyle="black"
-        c.rect(this.position.x,this.position.y,this.size.width,this.size.height);
+        c.fillStyle=this.color;
+        c.rect(this.position.x,this.position.y,this.size,this.size);
         c.fill();
     }
-    moveUp(){
-        this.velocity.y=-5;
-        this.position.y +=this.velocity.y;
-        
+   jump(){
+        if(this.jumpPressed){
+            this.velocity= this.velocity*this.acceleration;
+            this.position.y-=this.velocity*this.acceleration;
+            console.log("true")
+        }
+   }
+   down(){
+    if(this.jumpPressed ==false){
+        this.velocity= this.velocity*this.acceleration;
+        this.position.y+=this.velocity*this.acceleration;
     }
+                
+   }
     update(){
         this.draw();
-        this.moveUp();
+        
     }
 } 
-let obj = new Dragon();
-
+let player = new Dragon(150,500,50,"black");
+let enemy = new Enemy();
 function animate(){
     c.clearRect(0,0,canvas.width,canvas.height);
-    obj.update();
     background();
+    player.draw();
+    enemy.update();
+    
     requestAnimationFrame(animate);
 }
 
 animate();
+
+addEventListener("keydown",event =>{
+    if(event.code==="Space"){
+     player.jumpPressed=true;
+     player.jump();
+    }
+});
+addEventListener("keyup",event =>{
+    if(event.code ==="Space"){
+        player.jumpPressed=false;
+        player.down();
+    }
+})
+    
 
 
